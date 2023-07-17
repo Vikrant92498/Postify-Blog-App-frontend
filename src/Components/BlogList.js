75c6cacd7d3fddfb2e37a53a818c278435a6a95f
vikrant <vikrantkumar92498@gmail.com>
2023-07-17 17:39:02 +0530

    bloglist component done

---
+++ src/Components/BlogList.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import BlogBox from './BlogBox'
+import '../css/BlogList.css'
+const BlogList = () => {
+    const [blogs,setBlogs] = useState();
+    useEffect(()=>{
+        axios.get('http://localhost:5000/api/posts')
+        .then(blog=>{
+            setBlogs(blog.data)
+        })
+        .catch(err=>{
+            console.log(err);
+        })
+    },[])
+  return (
+    <div className='blog-container'>
+      {blogs?(
+         blogs.map(blog => (
+            <BlogBox
+              key={blog.id}
+              id={blog._id}
+              title={blog.title}
+              author={blog.author}
+              content={blog.content.substring(0, 50)}
+            />
+          ))
+      ):(<div>Fetching</div>)}
+    </div>
+  )
+}
+
+export default BlogList
